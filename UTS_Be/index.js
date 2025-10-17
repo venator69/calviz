8ba3357a2cd5aac91ce5f2e24261fbc8c80c3ad4
Hubert <dennishubert69420@gmail.com>
2025-10-17 21:30:39 +0700

    added backend for login

--- UTS_Be/index.js
+++ UTS_Be/index.js
@@ -6,9 +6,15 @@ const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
+
+
 const saltRounds = 10;
 const app = express();
 app.use(cors({  origin: '*'}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 dotenv.config({ path: './.env' });
 
@@ -17,7 +23,7 @@ const port = process.env.PORT || 3000;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'mydb',
+  database: 'logintest',
   password: 'yaldabaoth',
   port: 5432
 });
@@ -37,26 +43,46 @@ app.post('/register', upload.single('profile'), async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const profileFile = req.file;
-
-    if (!profileFile) {
-      return res.status(400).json({ status: 'error', message: 'Profile image is required' });
-    }
-
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashed = await bcrypt.hash(password, saltRounds);
 
     // Insert into database, storing profile as bytea
     const query = `
-      INSERT INTO users(name, email, password, profile)
-      VALUES($1, $2, $3, $4) RETURNING id
+      insert into users(name, email, password, profile)
+      values($1, $2, $3, $4) returning id
     `;
-    const values = [name, email, hashedPassword, profileFile.buffer]; // buffer contains bytes
+    const values = [name, email, hashed, profileFile.buffer]; // buffer contains bytes
     const result = await pool.query(query, values);
 
     res.status(200).json({ status: 'success', userId: result.rows[0].id });
   } catch (err) {
     console.error(err);
     res.status(500).json({ status: 'error', message: 'Database error' });
+  }
+});
+
+// login endpoint
+app.post('/login', upload.none(), async (req, res) => {
+  try {
+    const {name, password} = req.body;
+
+    // select from Database
+    const query = `
+      select * from users
+      where name = $1
+    `;
+    const result = await pool.query(query, [name]);
+
+    // Compare passwords
+    const match = await bcrypt.compare(password, result.rows[0].password);
+    if (!match) {
+      return res.status(401).json({ status: 'error', message: 'Incorrect password' });
+    }
+
+    res.json({ status: 'success', message: 'Login successful', result });
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).json({ status: 'error', message: 'Internal server error' });
   }
 });
 
